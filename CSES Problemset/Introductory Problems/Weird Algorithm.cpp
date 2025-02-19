24a0a5fc3d6deae770eb1907edcf8645f5bc4418
DenimFlame12 <113300417+DenimFlame12@users.noreply.github.com>
2025-02-19 18:35:14 +0530

    Update Weird Algorithm.cpp

--- CSES Problemset/Introductory Problems/Weird Algorithm.cpp
+++ CSES Problemset/Introductory Problems/Weird Algorithm.cpp
@@ -1,1 +1,26 @@
+/*
+*   author: denimflame12
+*   created: 19.02.2025 16:48:55
+*/
+ 
 #include <bits/stdc++.h>
+ 
+using namespace std;
+#define ll long long int
+ 
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll n;
+    cin >> n; 
+    cout << n << " ";
+    while(n != 1) {
+        if(n % 2) {
+            n *= 3; n++;
+        }
+        else {
+            n /= 2;
+        }
+        cout << n << " ";
+    }
+}
